930015028d53e8532af4589aeebb0401b5dd0991
Ayush Mishra <ayushmishra270306@gmail.com>
2026-02-11 08:19:16 +0530

    Time: 0 ms (100%), Space: 18.5 MB (14.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,13 +1,12 @@
-class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-         int n=image.size();
+ class Solution {
+    private:
+    void bfs(vector<vector<int>>& image,int sr,int sc,int color,vector<vector<int>>& vis,int originalval){
+          int n=image.size();
          int m=image[0].size();
-         int originalcolor=image[sr][sc];
-         if(originalcolor == color) return image;
+        vis[sr][sc]=1;
         queue<pair<int,int>>q;
+        image[sr][sc]=color;
         q.push({sr,sc});
-        image[sr][sc] = color;
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
         while(!q.empty()){
@@ -17,13 +16,22 @@ public:
                for(int i=0;i<4;i++){
                 int nrow=row+drow[i];
                 int ncol=col+dcol[i];
-                if(nrow>=0 &&nrow<n && ncol>=0 && ncol<m  && image[nrow][ncol]==originalcolor){
-                  
+                if(nrow>=0 &&nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==originalval){
+                    vis[nrow][ncol]=1;
                     image[nrow][ncol]=color;
                     q.push({nrow,ncol});
                 }
 }
+        }
+
     }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         int n=image.size();
+         int m=image[0].size();
+         int valcolor=image[sr][sc];
+         vector<vector<int>>vis(n,vector<int>(m,0));
+        bfs(image,sr,sc,color,vis,valcolor);
         return image;
     }
 };
