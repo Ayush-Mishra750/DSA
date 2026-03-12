836fb862e65bcf56edc51a9c3848a32caacfa58a
Ayush Mishra <ayushmishra270306@gmail.com>
2026-03-12 23:36:34 +0530

    Time: 1 ms (73.89%), Space: 18.3 MB (48.02%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        //first we create the adjacency matrix
+      vector<vector<pair<int,int>>> adj(n);
+        for(auto it :flights){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        queue<pair<int,pair<int,int>>>q;
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        q.push({0,{src,0}});
+        while(!q.empty()){
+           int stop=q.front().first;
+           int node=q.front().second.first;
+           int cost=q.front().second.second;
+                q.pop();
+                 
+           for(auto it:adj[node]){
+            if(stop>k)continue;
+                   if(stop+1<=k+1){
+                         if(dist[it.first]>cost+it.second){
+                            dist[it.first]=cost+it.second;
+                            q.push({stop+1,{it.first,cost+it.second}});
+                         }
+                   }
+           }
+
+        }
+        if(dist[dst]==1e9)return -1;
+        return dist[dst];
+    }
+};
