d9c7e1de6cfc9958195930e8b22cbd2191ef53d8
Ayush Mishra <ayushmishra270306@gmail.com>
2026-03-31 01:18:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.68%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,56 @@
+class DisjointSet{
+    public:
+    vector<int>size,parent;
+
+    DisjointSet(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    
+    //!Find ultimate parent of a node
+    int findUparent(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findUparent(parent[node]);
+    }
+    void unionBySize(int u,int v){
+        int ulp_u=findUparent(u);
+        int ulp_v=findUparent(v);
+        if(ulp_u==ulp_v)return;
+        if(size[ulp_u]<size[ulp_v]){
+           parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+       
+    }
+};
 class Solution {
-    private:
-    void dfs(int node,vector<vector<int>>& adj,vector<bool>&vis){
-        vis[node]=1;
-        for(int i=0;i<adj.size();i++){
-            if(adj[node][i]==1 &&!vis[i]){
-                dfs(i,adj,vis);
+public:
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    ds.unionBySize(i,j);
+                }
             }
         }
-      
-    }
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        int ans=0;
-        vector<bool>vis(n+1,0);
+        int cnt=0;
         for(int i=0;i<n;i++){
-               if(!vis[i]){
-                dfs(i,isConnected,vis);
-                ans++;
-               }
+            if(ds.findUparent(i)==i){
+                cnt++;
+            }
         }
-        return ans;
+        return cnt;
     }
 };
