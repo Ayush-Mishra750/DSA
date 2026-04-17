0a7a9cd807da454eb2b5b95ad2a416deac488a07
Ayush Mishra <ayushmishra270306@gmail.com>
2026-04-17 06:10:47 +0530

    Time: 242 ms (35.68%), Space: 235.1 MB (15.08%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -4,24 +4,28 @@ public:
         int n=nums.size();
         vector<int>ans(n,-1);
         unordered_map<int,vector<int>>mpp;
-        for(int i=0;i<nums.size();i++){
-             mpp[nums[i]].push_back(i);
+
+        for(int i=0;i<n;i++){
+            mpp[nums[i]].push_back(i);
         }
+
         for(auto it:mpp){
-            vector<int>&pos=it.second;
+            auto& pos=it.second;
             int m=pos.size();
             if(m==1)continue;
             for(int i=0;i<m;i++){
                 int curr=pos[i];
                 int prev=pos[(i-1+m)%m];
-                int next=pos[(i+1)%m];
                 int distprev=abs(curr-prev);
                 distprev=min(distprev,n-distprev);
+                int next=pos[(i+1)%m];
                 int distnext=abs(curr-next);
                 distnext=min(distnext,n-distnext);
+
                 ans[curr]=min(distprev,distnext);
+
             }
-           
+            
         }
         vector<int>res;
         for(auto it:queries){
