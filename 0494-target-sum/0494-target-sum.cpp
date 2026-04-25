c5a6b449e13ab5c8a75614d4f40eb021ce706a07
Ayush Mishra <ayushmishra270306@gmail.com>
2026-04-25 06:35:24 +0530

    Time: 6 ms (68.06%), Space: 15.7 MB (39.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int canPartition(vector<int>& nums,int t) {
+    int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        if((sum+t)%2!=0|| abs(t)>sum)return false;
-      int newtarget=(sum+t)/2;
-        vector<vector<int>>dp(n+1,vector<int>(newtarget+1,0));
-        for(int i=0;i<=n;i++)
-           dp[i][0]=1;
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if((sum+target)%2!=0|| abs(target)>sum)return 0;
 
-           for(int i=1;i<=n;i++){
+        int newtarget=(sum+target)/2;
+        vector<vector<int>>dp(n+1,vector<int>(newtarget+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
             for(int j=0;j<=newtarget;j++){
                 dp[i][j]=dp[i-1][j];
                 if(nums[i-1]<=j){
                     dp[i][j]+=dp[i-1][j-nums[i-1]];
                 }
             }
-           }
-           return dp[n][newtarget];
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return canPartition(nums,target);
+        }
+        return dp[n][newtarget];
+
     }
 };
