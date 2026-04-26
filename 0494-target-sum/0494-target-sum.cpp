54d63b681484d91f084c0af5b618b185b8172593
Ayush Mishra <ayushmishra270306@gmail.com>
2026-04-26 18:50:10 +0530

    Time: 3 ms (84.64%), Space: 15.6 MB (42.27%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,22 +3,23 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
-        if((sum+target)%2!=0|| abs(target)>sum)return 0;
 
+      if((sum+target)%2!=0 ||abs(target)>sum)return 0;
         int newtarget=(sum+target)/2;
-        vector<vector<int>>dp(n+1,vector<int>(newtarget+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=newtarget;j++){
-                dp[i][j]=dp[i-1][j];
-                if(nums[i-1]<=j){
-                    dp[i][j]+=dp[i-1][j-nums[i-1]];
-                }
+      vector<vector<int>>dp(n+1,vector<int>(newtarget+1,0));
+
+      for(int i=0;i<n;i++){
+        dp[i][0]=1;
+      }
+
+      for(int i=1;i<=n;i++){
+        for(int j=0;j<=newtarget;j++){
+            dp[i][j]=dp[i-1][j];
+            if(nums[i-1]<=j){
+                dp[i][j]+=dp[i-1][j-nums[i-1]];
             }
         }
-        return dp[n][newtarget];
-
+      }
+      return dp[n][newtarget];
     }
 };
