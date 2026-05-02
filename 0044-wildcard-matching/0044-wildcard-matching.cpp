8ea8ae408a8cb7ddc22ea5f809c8edcf9d701fab
Ayush Mishra <ayushmishra270306@gmail.com>
2026-05-02 06:42:48 +0530

    Time: 51 ms (52.37%), Space: 54.7 MB (14.49%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+// dp[i][j] = dp[i][j-1]   // * = empty
+//          OR dp[i-1][j]  // * = consume char
+    bool isMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,false));
+           dp[0][0]=true;
+        for(int i=1;i<=m;i++){
+            if(p[i-1]=='*'){
+                dp[0][i]=dp[0][i-1];
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1]||p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1]; //digonal
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]=dp[i][j-1] ||dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
