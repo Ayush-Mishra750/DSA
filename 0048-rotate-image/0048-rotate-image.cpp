4d72459caea0f4781e4df292c37b76d65cc15eea
Ayush Mishra <ayushmishra270306@gmail.com>
2026-08-25 11:10:00 +0530

    Time: 0 ms (100%), Space: 10.1 MB (93.65%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
+void reverse_find(vector<vector<int>>&matrix,int i){
+    int l=0;
+    int r=matrix[0].size()-1;
+    while(l<r){
+        int temp=matrix[i][r];
+        matrix[i][r]=matrix[i][l];
+        matrix[i][l]=temp;
+        l++;
+        r--;
+    }
+}
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
+       int m=matrix[0].size();
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+          swap(matrix[i][j],matrix[j][i]);
         }
-        for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+       }
+       for(int i=0;i<n;i++){
+        reverse_find(matrix,i);
+       }
     }
 };
