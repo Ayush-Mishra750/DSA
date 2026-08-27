044e30f1995a1e025177299caeefb34c97ec9793
Ayush Mishra <ayushmishra270306@gmail.com>
2026-08-28 01:07:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int total=m+n-2;
-        int mini=min(m-1,n-1);
-     long long ans=1;
-        for(int i=1;i<=mini;i++){
-            ans=ans*(total-mini+i)/i;
-        }
-        return ans;
+  long long nCr(int n, int r){
+    if(r > n-r) r = n-r; // optimize — chhota r lo
+
+    long long result = 1;
+
+    for(int i = 0; i < r; i++){
+        result *= (n - i);
+        result /= (i + 1);
     }
+
+    return result;
+}
+
+int uniquePaths(int m, int n){
+    return nCr(m+n-2, n-1);
+}
 };
